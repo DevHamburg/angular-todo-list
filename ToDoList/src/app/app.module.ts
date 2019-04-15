a13afd52f98bb13b94d6a109f1f8e65df0488a30
DevHamburg <guberinic040@gmail.com>
2019-04-15 14:38:01 +0200

    Add html elements and Styling

--- ToDoList/src/app/app.module.ts
+++ ToDoList/src/app/app.module.ts
@@ -6,19 +6,16 @@ import { PageListComponent } from './page-list/page-list.component';
 import { TemplateTodoComponent } from './_template/template-todo/template-todo.component';
 import { TemplateTodoFormComponent } from './_template/template-todo-form/template-todo-form.component';
 import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     PageListComponent,
     TemplateTodoComponent,
     TemplateTodoFormComponent,
-    TemplateHeaderComponent
+    TemplateHeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
